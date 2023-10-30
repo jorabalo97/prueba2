10d66ab400878ffb854c5bd21587cbd30ef87824
jorgeabalo97 <jorabalo97@gmail.com>
2023-10-30 14:09:51 +0100

    he cambiado unicamente el tiempo de la animacion y el color del background de la funcion selecOperation

--- iOS-Calculator/Util/UIButtonExtension.swift
+++ iOS-Calculator/Util/UIButtonExtension.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-private let orange = UIColor(red: 254/255, green: 148/255, blue: 0/255, alpha: 1)
+private let orange = UIColor(red): 254/255, green: 148/255, blue: 0/255, alpha: 1)
 
 extension UIButton {
     
@@ -20,10 +20,10 @@ extension UIButton {
     
     // Brilla
     func shine() {
-        UIView.animate(withDuration: 0.1, animations: {
+        UIView.animate(withDuration: 0.2, animations: {
             self.alpha = 0.5
         }) { (completion) in
-            UIView.animate(withDuration: 0.1, animations: {
+            UIView.animate(withDuration: 0.2, animations: {
                 self.alpha = 1
             })
         }
@@ -31,7 +31,7 @@ extension UIButton {
     
     // Apariencia selección botón de operación
     func selectOperation(_ selected: Bool) {
-        backgroundColor = selected ? .white : orange
+        backgroundColor = selected ? .white : blue
         setTitleColor(selected ? orange : .white, for: .normal)
     }
 
